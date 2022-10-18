93f78537c3a6a46b463f3658ed9732cd89a2412e
agonzg40 <agonzg40@estudiantes.unileon.es>
2022-10-18 13:07:52 +0200

    Hecho el ejercicio

--- exampleFunction.cpp
+++ exampleFunction.cpp
@@ -15,12 +15,26 @@ unsigned int wrapFunctionAdd(unsigned int ui_a, unsigned int ui_b) {
 }
 
 unsigned int wrapFunctionMul(unsigned int ui_a, unsigned int ui_b) {
-  unsigned umul = ui_a;
-  umul *= ui_b;
+  unsigned umul = 0;
+
+  if(UINT_MAX / ui_a < ui_b){
+    //Error
+  }else {
+    umul = ui_a*ui_b;
+  }
+
   return umul;
 }
 
+
 uint32_t wrapFunctionShift(uint32_t ui_a, unsigned int ui_b) {
-  uint32_t uShift = ui_a << ui_b | ui_a >> (32 - ui_b); 
+  uint32_t uShift = 0; 
+
+  if(ui_b >= UINT_MAX){
+    //Error
+  }else{
+    uShift = ui_a << ui_b | ui_a >> (32 - ui_b); 
+  }
+
   return uShift;
 }
